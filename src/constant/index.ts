eda6fcfd0382a84b4ebd2e0f78457eff90edebdf
toktar <renata.toktar@dsr-corporation.com>
2022-04-27 22:44:48 +0300

    change the explorer link

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -10,7 +10,7 @@ export const CHEQ_LEDGER_APP_INSTALL_LINK = 'https://github.com/cheqd/ledger-app
 export const COSMOS_LEDGER_APP_INSTALL_LINK =
 	'https://support.ledger.com/hc/en-us/articles/360013713840-Cosmos-ATOM-?docs=true';
 export const KEPLR_INSTALL_LINK = 'https://keplr.app';
-export const CHEQ_EXPLORER = IS_TESTNET ? 'https://testnet-explorer.cheqd.io/' : 'https://explorer.cheqd.net';
+export const CHEQ_EXPLORER = IS_TESTNET ? 'https://testnet-explorer.cheqd.io/' : 'https://explorer.cheqd.io';
 export const CHEQ_WALLET = IS_TESTNET ? 'https://wallet.testnet.cheqd.network' : 'https://wallet.cheqd.net';
 export const CHEQ_COINGECKO_ID = 'cheqd-network';
 
