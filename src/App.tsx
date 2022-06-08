d6a15a158ca2c571b59c8bd23e0240d4fecad6e6
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2022-06-08 18:07:34 +0300

    feat: Add state persistence for the identity page

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,17 @@ import store from './redux/store';
 
 import './locales';
 
+import { getPersistor } from "@rematch/persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+const persistor = getPersistor();
+
 const App = (): JSX.Element => (
 	<Provider store={store}>
-		<Core />
-		<ToastContainer closeButton={ToastCloseButton} hideProgressBar position="bottom-right" draggable={false} />
+		<PersistGate persistor={persistor}>
+			<Core />
+			<ToastContainer closeButton={ToastCloseButton} hideProgressBar position="bottom-right" draggable={false} />
+		</PersistGate>
 	</Provider>
 );
 
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,18 +1,30 @@
 import { RematchDispatch, RematchRootState, init } from '@rematch/core';
 import loadingPlugin, { ExtraModelsFromLoading } from '@rematch/loading';
 
+import persistPlugin from "@rematch/persist";
+import storage from "redux-persist/lib/storage";
+
 import { RootModel, reduxModels } from 'models';
 
 type FullModel = ExtraModelsFromLoading<RootModel, { type: 'full' }>;
 
 const store = init<RootModel, FullModel>({
-	models: reduxModels,
-	redux: {
-		rootReducers: {
-			LOGOUT: () => undefined,
-		},
-	},
-	plugins: [loadingPlugin({ type: 'full' })],
+    models: reduxModels,
+    redux: {
+        rootReducers: {
+            LOGOUT: () => undefined,
+        },
+    },
+    plugins: [
+        loadingPlugin({
+            type: 'full'
+        }),
+        persistPlugin({
+            key: "root",
+            storage,
+            whitelist: ['identity']
+        })
+    ],
 });
 
 export type Store = typeof store;
