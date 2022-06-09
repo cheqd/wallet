fca5ae142090ca4b6b32c0f3bc174594f9d30f0b
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-06-09 18:05:43 +0530

    feat: Twitter login with Auth0
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

---
+++ src/components/Buttons/Auth0SignIn.tsx
@@ -0,0 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import { useTranslation } from "react-i18next";
+import Button from "./Button";
+
+const Auth0SignIn = () => {
+	const { t } = useTranslation();
+	const { loginWithPopup } = useAuth0();
+
+	return <Button className="px-5" onClick={() => loginWithPopup()}>
+		{t('identity.twitter.connect')}
+	</Button>
+}
+
+export default Auth0SignIn;
