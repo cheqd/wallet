6dbe75d856dca5eeaad4070f218757f7ad44be68
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-09-23 17:55:04 +0530

    fix: Build

--- src/index.tsx
+++ src/index.tsx
@@ -18,6 +18,10 @@ root.render(
 	</React.StrictMode>,
 );
 
+if (import.meta.hot) {
+	import.meta.hot.accept();
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
