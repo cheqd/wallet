cc223956aab021636bca0e906206d64ff567f814
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-11-29 17:38:12 +0000

    Remove unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,19 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import alias from '@rollup/plugin-alias'
-import { fileURLToPath } from 'url';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import imagePlugin from '@rollup/plugin-image';
-import inject from '@rollup/plugin-inject'
-import stdLibBrowser from 'node-stdlib-browser';
+import inject from '@rollup/plugin-inject';
 
-
-function getPolyfillsForEnv() {
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
 		viteTsconfigPaths(),
+		splitVendorChunkPlugin(),
 		svgrPlugin(),
 		alias(),
 		imagePlugin(),
-		splitVendorChunkPlugin(),
 		{
 			...inject({
 				global: [
